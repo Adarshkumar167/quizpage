efe290efd010c71f70f1b438ff854b0754dc1148
Adarsh <adarshkumar167@gmail.com>
2023-06-30 23:22:43 +0530

    submit_page_80percent

--- lib/other/submit_page.dart
+++ lib/other/submit_page.dart
@@ -27,78 +27,323 @@ class SummaryPage extends StatelessWidget {
           )
         ],
       ),
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: 0.8 * MediaQuery.of(context).size.width,
-              alignment: Alignment.bottomCenter,
-              margin: const EdgeInsets.only(top: 20),
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 2,
-                    blurRadius: 5,
-                    offset: const Offset(0, 3),
-                  ),
-                ],
-              ),
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: MediaQuery.of(context).size.width * 0.1),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Stack(
-                            alignment: Alignment.center,
-                            children: [
-                              Image.asset(
-                                'assets/image/summary_page/medal_2.png',
-                                width: MediaQuery.of(context).size.width * 0.3,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                width: 0.8 * MediaQuery.of(context).size.width,
+                alignment: Alignment.bottomCenter,
+                margin: const EdgeInsets.only(top: 10),
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal:
+                                MediaQuery.of(context).size.width * 0.1),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Stack(
+                              alignment: Alignment.center,
+                              children: [
+                                Image.asset(
+                                  'assets/image/summary_page/medal_2.png',
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.3,
+                                ),
+                                Transform.translate(
+                                  offset: const Offset(-90, -65),
+                                  child: Image.asset(
+                                    'assets/image/summary_page/star1.png',
+                                    width: 70,
+                                  ),
+                                ),
+                                Transform.translate(
+                                  offset: const Offset(100, 35),
+                                  child: Image.asset(
+                                    'assets/image/summary_page/star2.png',
+                                    width: 90,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const Text(
+                              'Silver Medal',
+                              style: TextStyle(
+                                fontSize: 30,
+                                color: Color.fromRGBO(62, 73, 79, 1),
+                                fontWeight: FontWeight.bold,
+                                shadows: [
+                                  Shadow(
+                                    color: Color.fromRGBO(0, 0, 0, 0.25),
+                                    offset: Offset(2, 2),
+                                    blurRadius: 3,
+                                  ),
+                                ],
                               ),
-                              Transform.translate(
-                                offset: const Offset(-90, -65),
-                                child: Image.asset(
-                                  'assets/image/summary_page/star1.png',
-                                  width: 70,
+                            ),
+                            const SizedBox(height: 10),
+                            Container(
+                              height: 5,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                color: const Color.fromRGBO(62, 73, 79, 0.5),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            const SizedBox(height: 30),
+                          ],
+                        ),
+                      ),
+                      const Text(
+                        'We have completed Quiz successfully',
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 30),
+                        child: RichText(
+                          textAlign: TextAlign.center,
+                          text: TextSpan(
+                            children: [
+                              const TextSpan(
+                                text: 'You attempt ',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.black,
                                 ),
                               ),
-                              Transform.translate(
-                                offset: const Offset(95, 35),
-                                child: Image.asset(
-                                  'assets/image/summary_page/star2.png',
-                                  width: 90,
+                              TextSpan(
+                                text: '$marks questions',
+                                style: const TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.black,
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
+                              const TextSpan(
+                                text: ' and from that ',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              TextSpan(
+                                text: '$marks',
+                                style: const TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.green,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              const TextSpan(
+                                text: ' answers',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const TextSpan(
+                                text: ' is correct',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.black,
                                 ),
                               ),
                             ],
                           ),
-                          const SizedBox(height: 8),
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
                           const Text(
-                            'Join our daily challenge and win special gifts just for you',
+                            'Share with us: ',
                             style: TextStyle(
-                                fontSize: 14,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold),
+                              fontSize: 12,
+                            ),
+                          ),
+                          const SizedBox(width: 5),
+                          Image.asset(
+                            'assets/image/summary_page/facebook.png',
+                            scale: 4,
+                          ),
+                          const SizedBox(width: 10),
+                          Image.asset(
+                            'assets/image/summary_page/whatsapp.png',
+                            scale: 4,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Completion',
+                            style: TextStyle(
+                                fontSize: 12,
+                                color: Color.fromRGBO(151, 152, 159, 1)),
+                          ),
+                          Text(
+                            '$marks%',
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
                           ),
                         ],
                       ),
                     ),
-                  ],
+                  ),
+                  const SizedBox(width: 10),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Correct Answer',
+                            style: TextStyle(
+                                fontSize: 12,
+                                color: Color.fromRGBO(151, 152, 159, 1)),
+                          ),
+                          Text(
+                            '$marks questions',
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Skipped',
+                            style: TextStyle(
+                                fontSize: 12,
+                                color: Color.fromRGBO(151, 152, 159, 1)),
+                          ),
+                          Text(
+                            '10-$marks',
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Incorrect Answer',
+                            style: TextStyle(
+                                fontSize: 12,
+                                color: Color.fromRGBO(151, 152, 159, 1)),
+                          ),
+                          Text(
+                            '$marks%',
+                            style: const TextStyle(
+                                fontSize: 20, color: Colors.black),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 30),
+              SizedBox(
+                width: 0.8 * MediaQuery.of(context).size.width,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromRGBO(90, 216, 224, 1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    padding: EdgeInsets.zero,
+                  ),
+                  child: const Text(
+                    'DONE',
+                    style: TextStyle(color: Colors.white, fontSize: 12),
+                  ),
                 ),
               ),
-            ),
-          ],
+              const SizedBox(height: 10),
+            ],
+          ),
         ),
       ),
     );
